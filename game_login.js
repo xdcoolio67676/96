fff1b59eb35e966fe23aff513ad88cf249928c6f
xdcoolio67676 <113389415+xdcoolio67676@users.noreply.github.com>
2023-03-16 16:19:08 -0400

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,12 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+    //Set "player1_name" using localStorage.setItem() function
+    player1_name = localStorage.setItem()
+	  //Set "player2_name" using localStorage.setItem() function
+player2_name=localStorage.setItem()
+    window.location = "game_page.html";
+}
+
